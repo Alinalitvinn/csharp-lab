37ed90b47288aba54526e582857780fc5c8fc86f
Alinalitvinn <alyalitvin457@gmail.com>
2024-05-30 23:05:12 +0300

    upload updated test with project

---
+++ Application/Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MemoryDemo
+{
+    // Value type
+    public struct ValueType
+    {
+        public int Value;
+    }
+
+    // Reference type
+    public class ReferenceType
+    {
+        public int Value;
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value type demo
+            ValueType value1 = new ValueType { Value = 42 };
+            Console.WriteLine($"Value1: {value1.Value}"); // 42
+
+            PassByValue(value1);
+            Console.WriteLine($"Value1 after PassByValue: {value1.Value}"); // 42
+
+            // Reference type demo
+            ReferenceType ref1 = new ReferenceType { Value = 42 };
+            Console.WriteLine($"Ref1: {ref1.Value}"); // 42
+
+            PassByReference(ref ref1);
+            Console.WriteLine($"Ref1 after PassByReference: {ref1.Value}"); // 100
+
+            // Stack vs Heap demo
+            ValueType valueOnStack;  // Value type on stack
+            ReferenceType refOnHeap = new ReferenceType(); // Reference type on heap
+
+            // Garbage Collector demo
+            ReferenceType refForGC = new ReferenceType();
+            refForGC = null; // Make the object eligible for GC
+            GC.Collect(); // Request a garbage collection
+        }
+
+        public static void PassByValue(ValueType value)
+        {
+            value.Value = 100;
+            Console.WriteLine($"Value in PassByValue: {value.Value}"); // 100
+        }
+
+        public static void PassByReference(ref ReferenceType refParam)
+        {
+            refParam.Value = 100;
+            Console.WriteLine($"RefParam in PassByReference: {refParam.Value}"); // 100
+        }
+    }
+}
---
+++ Main.Test/Main.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MemoryDemo;
+using System.Collections.Generic;
+
+namespace Main.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PassByValue_ShouldNotModifyOriginalValue()
+        {
+            MemoryDemo.ValueType value = new MemoryDemo.ValueType { Value = 42 };
+            MemoryDemo.Program.PassByValue(value);
+            Assert.AreEqual(42, value.Value);
+        }
+
+        [TestMethod]
+        public void ValueTypeInstance_ShouldBeCreatedOnStack()
+        {
+            long initialHeapSize = GC.GetTotalMemory(true);
+
+            MemoryDemo.ValueType value;
+            value = new MemoryDemo.ValueType();
+
+            long finalHeapSize = GC.GetTotalMemory(true);
+
+            Assert.AreEqual(initialHeapSize, finalHeapSize, "ValueType instances should be created on the stack, not the heap.");
+        }
+
+        [TestMethod]
+        public void PassByReference_ShouldModifyOriginalValue()
+        {
+            MemoryDemo.ReferenceType refType = new MemoryDemo.ReferenceType { Value = 42 };
+            MemoryDemo.Program.PassByReference(ref refType);
+            Assert.AreEqual(100, refType.Value);
+        }
+
+        [TestMethod]
+        public void ReferenceTypeInstance_ShouldBeCreatedOnHeap()
+        {
+            long initialHeapSize = GC.GetTotalMemory(true);
+
+            MemoryDemo.ReferenceType refType = new MemoryDemo.ReferenceType();
+
+            long finalHeapSize = GC.GetTotalMemory(true);
+
+            Assert.IsTrue(finalHeapSize > initialHeapSize, "ReferenceType instances should be created on the heap.");
+        }
+
+        [TestMethod]
+        public void GarbageCollector_ShouldCollectUnusedObjects()
+        {
+            List<MemoryDemo.ReferenceType> objects = new List<MemoryDemo.ReferenceType>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                objects.Add(new MemoryDemo.ReferenceType());
+            }
+
+            long heapSizeBeforeGC = GC.GetTotalMemory(true);
+
+            objects.Clear();
+            GC.Collect();
+            long heapSizeAfterGC = GC.GetTotalMemory(true);
+
+            Assert.IsTrue(heapSizeAfterGC < heapSizeBeforeGC, "Heap size should decrease after garbage collection.");
+        }
+    }
+}
